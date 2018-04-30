443381fcdcf3f90947692ec05c4d3040668c5254
Kayo <kayoanderson0403@gmail.com>
2018-04-29 23:24:36 -0300

    Class 2 - Basically, some authentication with jsonwebtoken package

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const User = require('../models/users');
 
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
+
 const router = express.Router();
+
+function generateToken(params = {}) {
+	return jwt.sign(params, authConfig.secret, {
+		expiresIn: 86400,
+	});
+}
 
 router.post('/register', async(req, res) => {
 	const { email } = req.body;
@@ -15,10 +27,33 @@ router.post('/register', async(req, res) => {
 
 		user.password = undefined;
 
-		return res.send({ user });
+		return res.send({
+			user,
+			token: generateToken({ id: user.id }),
+		});
 	} catch (err) {
 		return ress.status(400).send({ error: 'Registration failed' });
 	}
+});
+
+router.post('/authenticate', async(req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user)
+		return res.status(400).send({ error: 'User not found' });
+
+	if (!await bcrypt.compare(password, user.password))
+		return res.status(400).send({ error: 'Invalid password' });
+
+	user.password = undefined;
+
+	res.send({ 
+		user,
+		token: generateToken({ id: user.id }),
+	});
+
 });
 
 module.exports = app => app.use('/auth', router);
